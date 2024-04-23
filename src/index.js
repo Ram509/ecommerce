c5e160cbfe381a45181031dc6b5b5a1dcda61e3f
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-04-23 21:12:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/Header.jsx
+import Login from "./components/Login.jsx
+import Logout from "./components/Logout.jsx
+import Navbar from "./components/Navbar.jsx
 
 import App from "./App.js";
 
