c20afda31030ca14c85c820de123066841f357a1
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-04-24 14:19:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/Header.jsx
-import Login from "./components/Login.jsx
-import Logout from "./components/Logout.jsx
-import Navbar from "./components/Navbar.jsx
 
 import App from "./App.js";
 
