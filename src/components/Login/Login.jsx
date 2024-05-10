914f21e73335b473fa0f3f4ecf89bf2fcb7a9c61
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-05-10 22:01:28 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import React from "react";
+function Login() {
+  const  [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const about = () => {
-  return <div>Login</div>;
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await axios.post('/api/users/login', {
+      email, 
+      password
+    });
+    console.log(response.data);
+  }catch (error) {
+    console.log(error);
+  }
 };
+ return (
 
+<form onSubmit={handleSubmit}
+  <label htmlfor="email">Email:</label>
+<input
+   type="email"
+   id="email"
+   value={email}
+   onChange={(e) => setEmail(e.target.value)}
+/>
+<br/>
+<label htmlFor="password">Password:</label>
+<input
+   type="password"
+   id="password"
+   value={password}
+   onChange={(e) => setPassword(e.target.value)}
+/>
+<br/>
+ <button type="submit">Login</button>
+</form>
+);
+}
 export default Login;
